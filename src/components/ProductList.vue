218b0a4562623e7b464c6f84805cc510f0b6d795
Oskar Wiiala <pullupmastery@gmail.com>
2023-07-13 12:26:11 +0300

    Lesson Vuex Map Helpers done

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,44 +1,70 @@
 <template>
   <div>
     <h1>Product List</h1>
-    <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif"/>
+    <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif" />
     <ul v-else>
       <li v-for="product in products" :key="product.key">
         {{ product.title }} - {{ product.price | currency }} - {{ product.inventory }}
-        <button 
-        :disabled="!productIsInStock(product)"
-        @click="addProductToCart(product)"
-        >Add to cart</button>
+        <button :disabled="!productIsInStock(product)" @click="addProductToCart(product)">Add to cart</button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+  import { mapState, mapGetters, mapActions } from 'vuex'
   export default {
     data () {
       return {
-        loading: false
+        loading: false,
+        producIndex: 1
       }
     },
+
+    // mapState accepts an array with the state properties to map, or an object
+    // when using an array, component's computed properties will have the same names as the state properties
+    // if you want the name to be different, pass an object
+    // when passing an object to mapState, functions can also be used as values instead of just using strings
+    // computed: mapState({
+    //   products: state => state.products, // where does state come from?
+    //   // firstProduct: state => state.products[0], // arrow function required
+    //   // specificProduct (state) {
+    //   //   return state.products[this.productIndex]
+    //   // }
+    // }),
 
     computed: {
-      products () {
-        // return this.$store.getters.availableProducts
-        // return store.state.products
+      ...mapState({
+        products: state => state.products
+      }),
 
-        return this.$store.state.products
-      },
-
-      productIsInStock () {
-        return this.$store.getters.productIsInStock
-      }
+      ...mapGetters({
+        productIsInStock: 'productIsInStock'
+      })
     },
 
+    // computed: {
+    //   products () {
+    //     // return this.$store.getters.availableProducts
+    //     // return store.state.products
+
+    //     return this.$store.state.products
+    //   },
+
+    //   productIsInStock () {
+    //     return this.$store.getters.productIsInStock
+    //   }
+    // },
+
     methods: {
-      addProductToCart (product) {
-        this.$store.dispatch('addProductToCart', product)
-      }
+      ...mapActions({
+        fetchProducts: 'fetchProducts',
+        addProductToCart: 'addProductToCart'
+      }),
+
+      // addProductToCart (product) {
+      //   this.$store.dispatch('addProductToCart', product)
+      // }
     },
 
     // Will run right after the instance is created
@@ -47,7 +73,7 @@
       // dispatch will call store action method
       // The first argument is the name of the action, the second is opitional, which is the payload
       // .then comes from the promise in the action method
-      this.$store.dispatch('fetchProducts').then((message) => {
+      this.fetchProducts().then((message) => {
         console.log(message);
         this.loading = false
       })
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -3,35 +3,46 @@
     <h1>Shopping Cart</h1>
     <ul>
       <li v-for="product in products" :key="product.id">
-      {{ product.title }} - {{ product.price | currency }} - {{ product.quantity }}</li>
+        {{ product.title }} - {{ product.price | currency }} - {{ product.quantity }}</li>
     </ul>
     <h1>Total: {{ total | currency }}</h1>
     <button @click="checkout">Checkout</button>
-    <p>Checkout status: {{ $store.state.checkoutStatus }}</p>
+    <p v-if="checkoutStatus">Checkout status: {{ checkoutStatus }}</p>
   </div>
 </template>
 
 <script>
-export default {
-  computed: {
-    products () {
-      return this.$store.getters.cartProducts
+  import { mapState, mapGetters, mapActions } from 'vuex';
+  export default {
+    computed: {
+      ...mapGetters({
+        products: 'cartProducts',
+        total: 'cartTotal'
+      }),
+
+      ...mapState({
+        checkoutStatus: 'checkoutStatus'
+      })
+
+      // products () {
+      //   return this.$store.getters.cartProducts
+      // },
+
+      // total () {
+      //   return this.$store.getters.cartTotal
+      // }
     },
 
-    total () {
-      return this.$store.getters.cartTotal
-    }
-  },
-
-  methods: {
-    checkout () {
-      console.log('atempting checkout');
-      this.$store.dispatch('checkout')
+    methods: {
+      ...mapActions({
+        checkout: 'checkout'
+      })
+      // checkout () {
+      //   console.log('atempting checkout');
+      //   this.$store.dispatch('checkout')
+      // }
     }
   }
-}
 </script>
 
-<style>
-
-</style>
+<style></style>
